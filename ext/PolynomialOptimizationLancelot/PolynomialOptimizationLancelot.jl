398f8311685dea0ff688d80cc4934742dac8a2bb
Benjamin Desef <projekter-git@yahoo.de>
2024-07-14 18:40:56 +0200

    Make sure Lancelot bindings are available

--- ext/PolynomialOptimizationLancelot/PolynomialOptimizationLancelot.jl
+++ ext/PolynomialOptimizationLancelot/PolynomialOptimizationLancelot.jl
@@ -8,6 +8,16 @@ import StaticPolynomials
 include("./Bindings.jl")
 include("./Lancelot.jl")
 
-__init__() = push!(Solver.solver_methods, :Lancelot)
+function __init__()
+    push!(Solver.solver_methods, :Lancelot)
+    # This weakdep introduces a new binding, which should be accessible and documented in the main package.
+    PolynomialOptimization.LANCELOT_simple = LANCELOT_simple
+    POmeta = Docs.meta(PolynomialOptimization)
+    POLmeta = Docs.meta(PolynomialOptimizationLancelot)
+    PObinding = Docs.Binding(PolynomialOptimization, :LANCELOT_simple)
+    POLbinding = Docs.Binding(PolynomialOptimizationLancelot, :LANCELOT_simple)
+    POmeta[PObinding] = POLmeta[POLbinding]
+    delete!(POLmeta, POLbinding)
+end
 
 end
