c33a392005eb3c116966a55a6bf14f5c8a88433e
Benjamin Desef <projekter-git@yahoo.de>
2024-12-25 14:01:36 +0100

    Mosek: Use macro calls more often (even the simple functions don't seem to be inlined) Maybe in the future bypass the Mosek API directly, which is very inefficient with its useless disable_sigints and creates boilerplate exception checks...

--- ext/PolynomialOptimizationMosek/MosekMoment.jl
+++ ext/PolynomialOptimizationMosek/MosekMoment.jl
@@ -14,7 +14,7 @@ end
 function Base.append!(state::StateMoment, key)
     if state.num_used_vars == state.num_solver_vars
         newnum = overallocation(state.num_solver_vars + one(state.num_solver_vars))
-        appendvars(state.task, newnum - state.num_solver_vars)
+        Mosek.@MSK_appendvars(state.task.task, newnum - state.num_solver_vars)
         state.num_solver_vars = newnum
     end
     return state.mon_to_solver[FastKey(key)] = let uv=state.num_used_vars
@@ -30,7 +30,7 @@ Solver.psd_indextype(::StateMoment) = PSDIndextypeVector(:L)
 function Solver.add_var_slack!(state::StateMoment, num::Int)
     if state.num_used_vars + num > state.num_solver_vars
         newnum = overallocation(state.num_used_vars + Int32(num))
-        appendvars(state.task, newnum - state.num_solver_vars)
+        Mosek.@MSK_appendvars(state.task.task, newnum - state.num_solver_vars)
         state.num_solver_vars = newnum
     end
     result = state.num_used_vars:state.num_used_vars+Int32(num -1)
@@ -39,52 +39,54 @@ function Solver.add_var_slack!(state::StateMoment, num::Int)
 end
 
 function Solver.add_constr_nonnegative!(state::StateMoment, indvals::Indvals{Int32,Float64})
-    appendcons(state.task, 1)
-    Mosek.@MSK_putarow(state.task.task, state.num_cons, length(indvals), indvals.indices, indvals.values)
-    Mosek.@MSK_putconbound(state.task.task, state.num_cons, MSK_BK_LO, 0., Inf)
+    task = state.task.task
+    Mosek.@MSK_appendcons(task, 1)
+    Mosek.@MSK_putarow(task, state.num_cons, length(indvals), indvals.indices, indvals.values)
+    Mosek.@MSK_putconbound(task, state.num_cons, MSK_BK_LO.value, 0., Inf)
     state.num_cons += one(state.num_cons)
     return
 end
 
 function Solver.add_constr_nonnegative!(state::StateMoment, indvals::IndvalsIterator{Int32,Float64})
-    N = length(indvals)
-    appendcons(state.task, N)
+    task = state.task.task
+    Mosek.@MSK_appendcons(task, length(indvals))
     id = state.num_cons
     curcon = id
     for indval in indvals
-        @capture(Mosek.@MSK_putarow(state.task.task, $curcon, length(indval), indval.indices, indval.values))
+        @capture(Mosek.@MSK_putarow(task, $curcon, length(indval), indval.indices, indval.values))
         curcon += one(curcon)
     end
-    Mosek.@MSK_putconboundsliceconst(state.task.task, id, curcon, MSK_BK_LO.value, 0., Inf)
+    Mosek.@MSK_putconboundsliceconst(task, id, curcon, MSK_BK_LO.value, 0., Inf)
     state.num_cons = curcon
     return
 end
 
 function Solver.add_constr_quadratic!(state::StateMoment, indvals::IndvalsIterator{Int32,Float64}, ::Val{rotated}=Val(false)) where {rotated}
+    task = state.task.task
     conedim = length(indvals)
-    appendafes(state.task, conedim)
+    Mosek.@MSK_appendafes(task, conedim)
     id = state.num_afes
     curafe = id
     for indval in indvals
-        @capture(Mosek.@MSK_putafefrow(state.task.task, $curafe, length(indval), indval.indices, indval.values))
+        @capture(Mosek.@MSK_putafefrow(task, $curafe, length(indval), indval.indices, indval.values))
         curafe += one(curafe)
     end
     state.num_afes = curafe
     # TODO: benchmark which approach is better, subsequence putafefrow as here or one putafefrowlist with preprocessing
-    if !rotated && length(indvals) == 3
-        Mosek.@MSK_appendaccseq(state.task.task, 1, 3, id, C_NULL)
-    elseif rotated && length(indvals) == 3
-        Mosek.@MSK_appendaccseq(state.task.task, 2, 3, id, C_NULL)
-    elseif rotated && length(indvals) == 4
-        Mosek.@MSK_appendaccseq(state.task.task, 3, 4, id, C_NULL)
+    if !rotated && conedim == 3
+        Mosek.@MSK_appendaccseq(task, 1, 3, id, C_NULL)
+    elseif rotated && conedim == 3
+        Mosek.@MSK_appendaccseq(task, 2, 3, id, C_NULL)
+    elseif rotated && conedim == 4
+        Mosek.@MSK_appendaccseq(task, 3, 4, id, C_NULL)
     else
         dom = Ref{Int64}()
         if rotated
-            Mosek.@MSK_appendrquadraticconedomain(state.task.task, N, dom)
+            Mosek.@MSK_appendrquadraticconedomain(task, conedim, dom)
         else
-            Mosek.@MSK_appendquadraticconedomain(state.task.task, N, dom)
+            Mosek.@MSK_appendquadraticconedomain(task, conedim, dom)
         end
-        Mosek.@MSK_appendaccseq(state.task.task, dom[], N, id, C_NULL)
+        Mosek.@MSK_appendaccseq(task, dom[], conedim, id, C_NULL)
     end
     return
 end
@@ -93,35 +95,38 @@ Solver.add_constr_rotated_quadratic!(state::StateMoment, indvals::IndvalsIterato
     add_constr_quadratic!(state, indvals, Val(true))
 
 function Solver.add_constr_psd!(state::StateMoment, dim::Int, data::IndvalsIterator{Int32,Float64})
+    task = state.task.task
     n = trisize(dim)
-    appendafes(state.task, n)
+    Mosek.@MSK_appendafes(task, n)
     curafe = state.num_afes
     # putafefrowlist would require us to create collect(state.num_afes:state.num_afes+length(data)) for the afeidx parameter
     # and accumulate Base.index_lengths(data) for ptrrow
     for indvals in data
-        @capture(Mosek.@MSK_putafefrow(state.task.task, $curafe, length(indvals), indvals.indices, indvals.values))
+        @capture(Mosek.@MSK_putafefrow(task, $curafe, length(indvals), indvals.indices, indvals.values))
         curafe += 1
     end
     # for sure, the number of PSD cones will not be the bottleneck, so let's save ourselves the trouble of caching already
     # existing domains
     cone = Ref{Int64}()
-    Mosek.@MSK_appendsvecpsdconedomain(state.task.task, n, cone)
-    Mosek.@MSK_appendaccseq(state.task.task, cone[], n, state.num_afes, C_NULL)
+    Mosek.@MSK_appendsvecpsdconedomain(task, n, cone)
+    Mosek.@MSK_appendaccseq(task, cone[], n, state.num_afes, C_NULL)
     state.num_afes = curafe
     return
 end
 
 function Solver.add_constr_fix_prepare!(state::StateMoment, num::Int)
-    appendcons(state.task, num)
+    task = state.task.task
+    Mosek.@MSK_appendcons(task, num)
     # most of the constraints will be zero, so we can save some calls if we all set them to zero and only change the few
     # nonzero ones
-    Mosek.@MSK_putconboundsliceconst(state.task.task, state.num_cons, state.num_cons + num, MSK_BK_FX, 0., 0.)
+    Mosek.@MSK_putconboundsliceconst(task, state.num_cons, state.num_cons + num, MSK_BK_FX.value, 0., 0.)
     return
 end
 
 function Solver.add_constr_fix!(state::StateMoment, ::Nothing, indvals::Indvals{Int32,Float64}, rhs::Float64)
-    Mosek.@MSK_putarow(state.task.task, state.num_cons, length(indvals), indvals.indices, indvals.values)
-    iszero(rhs) || Mosek.@MSK_putconbound(state.task.task, state.num_cons, MSK_BK_FX, rhs, rhs)
+    task = state.task.task
+    Mosek.@MSK_putarow(task, state.num_cons, length(indvals), indvals.indices, indvals.values)
+    iszero(rhs) || Mosek.@MSK_putconbound(task, state.num_cons, MSK_BK_FX.value, rhs, rhs)
     state.num_cons += one(state.num_cons)
     return
 end
@@ -137,20 +142,22 @@ function Solver.poly_optimize(::Val{:MosekMoment}, relaxation::AbstractRelaxatio
     setup_time = @elapsed begin
         K = _get_I(eltype(monomials(poly_problem(relaxation).objective)))
 
+        taskptr = task.task
         verbose && putstreamfunc(task, MSK_STREAM_LOG, printstream)
         for (k, v) in parameters
             putparam(task, string(k), string(v))
         end
-        putobjsense(task, MSK_OBJECTIVE_SENSE_MINIMIZE)
+        Mosek.@MSK_putobjsense(taskptr, MSK_OBJECTIVE_SENSE_MINIMIZE.value)
         # just set up some domains, this is cheap and we don't have to query them afterwards
-        appendrplusdomain(task, 1)
-        appendquadraticconedomain(task, 3)
-        appendrquadraticconedomain(task, 3)
-        appendrquadraticconedomain(task, 4)
+        domidx_ = Ref{Int64}()
+        Mosek.@MSK_appendrplusdomain(taskptr, 1, domidx_)
+        Mosek.@MSK_appendquadraticconedomain(taskptr, 3, domidx_)
+        Mosek.@MSK_appendrquadraticconedomain(taskptr, 3, domidx_)
+        Mosek.@MSK_appendrquadraticconedomain(taskptr, 4, domidx_)
 
         state = StateMoment{K}(task)
         moment_setup!(state, relaxation, groupings; representation)
-        Mosek.@MSK_putvarboundsliceconst(task.task, 0, state.num_used_vars, MSK_BK_FR.value, -Inf, Inf)
+        Mosek.@MSK_putvarboundsliceconst(taskptr, 0, state.num_used_vars, MSK_BK_FR.value, -Inf, Inf)
         customize(state)
     end
     @verbose_info("Setup complete in ", setup_time, " seconds")
--- ext/PolynomialOptimizationMosek/MosekSOS.jl
+++ ext/PolynomialOptimizationMosek/MosekSOS.jl
@@ -14,7 +14,7 @@ end
 function Base.append!(state::StateSOS, key)
     if state.num_used_cons == state.num_solver_cons
         newcon = overallocation(state.num_solver_cons + one(state.num_solver_cons))
-        appendcons(state.task, newcon - state.num_solver_cons)
+        Mosek.@MSK_appendcons(state.task.task, newcon - state.num_solver_cons)
         state.num_solver_cons = newcon
     end
     return state.mon_to_solver[FastKey(key)] = let uc=state.num_used_cons
@@ -30,7 +30,7 @@ Solver.psd_indextype(::StateSOS) = PSDIndextypeMatrixCartesian(:L, zero(Int32))
 function Solver.add_constr_slack!(state::StateSOS, num::Int)
     if state.num_used_cons + num > state.num_solver_cons
         newnum = overallocation(state.num_used_cons + Int32(num))
-        appendcons(state.task, newnum - state.num_solver_cons)
+        Mosek.@MSK_appendcons(state.task.task, newnum - state.num_solver_cons)
         state.num_solver_cons = newnum
     end
     result = state.num_used_cons:state.num_used_cons+Int32(num -1)
@@ -39,91 +39,86 @@ function Solver.add_constr_slack!(state::StateSOS, num::Int)
 end
 
 function Solver.add_var_nonnegative!(state::StateSOS, indvals::Indvals{Int32,Float64})
-    appendvars(state.task, 1)
-    Mosek.@MSK_putacol(state.task.task, state.num_vars, length(indvals), indvals.indices, indvals.values)
-    Mosek.@MSK_putvarbound(state.task.task, state.num_vars, MSK_BK_LO.value, 0., Inf)
+    task = state.task.task
+    Mosek.@MSK_appendvars(task, 1)
+    Mosek.@MSK_putacol(task, state.num_vars, length(indvals), indvals.indices, indvals.values)
+    Mosek.@MSK_putvarbound(task, state.num_vars, MSK_BK_LO.value, 0., Inf)
     state.num_vars += one(state.num_vars)
     return
 end
 
 function Solver.add_var_nonnegative!(state::StateSOS, indvals::IndvalsIterator{Int32,Float64})
-    N = length(indvals)
-    appendvars(state.task, N)
+    task = state.task.task
+    Mosek.@MSK_appendvars(task, length(indvals))
     id = state.num_vars
     curvar = id
     for indval in indvals
-        Mosek.@MSK_putacol(state.task.task, curvar, length(indval), indval.indices, indval.values)
+        @capture(Mosek.@MSK_putacol(task, $curvar, length(indval), indval.indices, indval.values))
         curvar += one(curvar)
     end
-    Mosek.@MSK_putvarboundsliceconst(state.task.task, id, curcon, MSK_BK_LO.value, 0., Inf)
+    Mosek.@MSK_putvarboundsliceconst(task, id, curcon, MSK_BK_LO.value, 0., Inf)
     state.num_vars = curvar
     return
 end
 
-function Solver.add_var_quadratic!(state::StateSOS, indvals::IndvalsIterator{Int32,Float64})
+function Solver.add_var_quadratic!(state::StateSOS, indvals::IndvalsIterator{Int32,Float64}, ::Val{rotated}=Val(false)) where {rotated}
+    task = state.task.task
     conedim = length(indvals)
     rhsdim = conedim -2
-    appendvars(state.task, conedim)
+    Mosek.@MSK_appendvars(task, conedim)
     id = state.num_vars
-    Mosek.@MSK_putvarbound(state.task.task, id, MSK_BK_LO.value, 0., Inf)
-    Mosek.@MSK_putvarboundsliceconst(state.task.task, id + one(id), id + Int32(conedim), MSK_BK_FR.value, -Inf, Inf)
-    Mosek.@MSK_appendconeseq(state.task.task, MSK_CT_QUAD.value, 0., conedim, id)
+    if rotated
+        Mosek.@MSK_putvarboundsliceconst(task, id, id + Int32(2), MSK_BK_LO.value, 0., Inf)
+    else
+        Mosek.@MSK_putvarbound(task, id, MSK_BK_LO.value, 0., Inf)
+    end
+    Mosek.@MSK_putvarboundsliceconst(task, id + Int32(rotated ? 2 : 1), id + Int32(conedim), MSK_BK_FR.value, -Inf, Inf)
+    Mosek.@MSK_appendconeseq(task, (rotated ? MSK_CT_RQUAD : MSK_CT_QUAD).value, 0., conedim, id)
     curvar = id
     for indval in indvals
-        Mosek.@MSK_putacol(state.task.task, curvar, length(indval), indval.indices, indval.values)
+        @capture(Mosek.@MSK_putacol(task, $curvar, length(indval), indval.indices, indval.values))
         curvar += one(curvar)
     end
     state.num_vars = curvar
     return
 end
 
-function Solver.add_var_rotated_quadratic!(state::StateSOS, indvals::IndvalsIterator{Int32,Float64})
-    conedim = length(indvals)
-    rhsdim = conedim -2
-    appendvars(state.task, conedim)
-    id = state.num_vars
-    Mosek.@MSK_putvarboundsliceconst(state.task.task, id, id + Int32(2), MSK_BK_LO.value, 0., Inf)
-    Mosek.@MSK_putvarboundsliceconst(state.task.task, id + Int32(2), id + Int32(conedim), MSK_BK_FR.value, -Inf, Inf)
-    Mosek.@MSK_appendconeseq(state.task.task, MSK_CT_RQUAD.value, 0., conedim, id)
-    curvar = id
-    for indval in indvals
-        Mosek.@MSK_putacol(state.task.task, curvar, length(indval), indval.indices, indval.values)
-        curvar += one(curvar)
-    end
-    state.num_vars = curvar
-    return
-end
+Solver.add_var_rotated_quadratic!(state::StateSOS, indvals::IndvalsIterator{Int32,Float64}) =
+    add_var_quadratic!(state, indvals, Val(true))
 
 function Solver.add_var_psd!(state::StateSOS, dim::Int, data::PSDMatrixCartesian{Int32,Float64})
-    Mosek.@MSK_appendbarvars(state.task.task, 1, Ref(Int32(dim)))
+    task = state.task.task
+    Mosek.@MSK_appendbarvars(task, 1, Ref(Int32(dim)))
     outidx = Ref{Int64}()
     oneref = Ref{Float64}(1.)
     @inbounds for (midx, (symrows, symcols, vals)) in data
-        Mosek.@MSK_appendsparsesymmat(state.task.task, dim, length(symrows), symrows, symcols, vals, outidx)
-        Mosek.@MSK_putbaraij(state.task.task, midx, state.num_bar_vars, 1, outidx, oneref)
+        Mosek.@MSK_appendsparsesymmat(task, dim, length(symrows), symrows, symcols, vals, outidx)
+        Mosek.@MSK_putbaraij(task, midx, state.num_bar_vars, 1, outidx, oneref)
     end
     state.num_bar_vars += one(state.num_bar_vars)
     return
 end
 
 function Solver.add_var_free_prepare!(state::StateSOS, num::Int)
-    appendvars(state.task, num)
-    Mosek.@MSK_putvarboundsliceconst(state.task.task, state.num_vars, state.num_vars + num, MSK_BK_FR.value, -Inf, Inf)
+    task = state.task.task
+    Mosek.@MSK_appendvars(task, num)
+    Mosek.@MSK_putvarboundsliceconst(task, state.num_vars, state.num_vars + num, MSK_BK_FR.value, -Inf, Inf)
     return
 end
 
 function Solver.add_var_free!(state::StateSOS, ::Nothing, indvals::Indvals{Int32,Float64}, obj::Float64)
-    Mosek.@MSK_putacol(state.task.task, state.num_vars, length(indvals), indvals.indices, indvals.values)
-    iszero(obj) || Mosek.@MSK_putcj(state.task.task, state.num_vars, obj)
+    task = state.task.task
+    Mosek.@MSK_putacol(task, state.num_vars, length(indvals), indvals.indices, indvals.values)
+    iszero(obj) || Mosek.@MSK_putcj(task, state.num_vars, obj)
     state.num_vars += 1
     return
 end
 
 function Solver.fix_constraints!(state::StateSOS, indvals::Indvals{Int32,Float64})
+    task = state.task.task
     len = length(indvals)
-    Mosek.@MSK_putconboundsliceconst(state.task.task, 0, state.num_used_cons, MSK_BK_FX, 0., 0.)
-    Mosek.@MSK_putconboundlist(state.task.task, len, indvals.indices, fill(MSK_BK_FX.value, len), indvals.values,
-        indvals.values)
+    Mosek.@MSK_putconboundsliceconst(task, 0, state.num_used_cons, MSK_BK_FX, 0., 0.)
+    Mosek.@MSK_putconboundlist(task, len, indvals.indices, fill(MSK_BK_FX.value, len), indvals.values, indvals.values)
     return
 end
 
@@ -137,7 +132,7 @@ function Solver.poly_optimize(::Val{:MosekSOS}, relaxation::AbstractRelaxation, 
         for (k, v) in parameters
             putparam(task, string(k), string(v))
         end
-        putobjsense(task, MSK_OBJECTIVE_SENSE_MAXIMIZE)
+        Mosek.@MSK_putobjsense(task.task, MSK_OBJECTIVE_SENSE_MAXIMIZE.value)
 
         state = StateSOS{K}(task)
         sos_setup!(state, relaxation, groupings; representation)
