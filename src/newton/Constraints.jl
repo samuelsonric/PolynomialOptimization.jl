a93da7550ee2c084c199e050a8f0c0d39d10fc32
Benjamin Desef <projekter-git@yahoo.de>
2025-04-18 18:34:49 +0200

    Introduce multithreading to Newton constraint merging

--- src/newton/Constraints.jl
+++ src/newton/Constraints.jl
@@ -1,3 +1,111 @@
+function grouping_loop(mons_idx_set, e, groupings, monₜ, ::Tuple{}, sync)
+    this_set = isnothing(sync) ? mons_idx_set : Set{FastKey{UInt}}()
+    for grouping in groupings, (i, g₁) in enumerate(grouping), g₂ in Iterators.take(grouping, i)
+        push!(this_set,
+            FastKey(monomial_index(e, g₁, monₜ, g₂)),
+            FastKey(monomial_index(e, g₂, monₜ, g₁))
+        )
+    end
+    if !isnothing(sync)
+        lock(sync)
+        try
+            union!(mons_idx_set, this_set)
+        finally
+            unlock(sync)
+        end
+    end
+    return
+end
+
+function grouping_loop(mons_idx_set, e, groupings, monₜ, (monₜe,)::Tuple{Any}, sync)
+    this_set = isnothing(sync) ? mons_idx_set : Set{FastKey{UInt}}()
+    for grouping in groupings, (i, g₁) in enumerate(grouping), g₂ in Iterators.take(grouping, i)
+        # While the prefactor is g₁*conj(g₂), we drop the conjugates anyway.
+        # Note that no prefactor has a single conjugate component; and while the monomials might have them, the
+        # conjugates will come up later anyway!
+        push!(this_set,
+            FastKey(exponents_sum(e, g₁, monₜe)), # KillConjugates(g₁*monₜ*ḡ₂)
+            FastKey(exponents_sum(e, g₂, monₜe)), # KillConjugates(g₂*monₜ*ḡ₁)
+        )
+    end
+    if !isnothing(sync)
+        lock(sync)
+        try
+            union!(gl.mons_idx_set, this_set)
+        finally
+            unlock(sync)
+        end
+    end
+    return
+end
+
+function grouping_loop(mons_idx_set, e, groupings, monₜ, (monₜe, monₜec)::Tuple{Any,Any}, sync)
+    this_set = isnothing(sync) ? mons_idx_set : Set{FastKey{UInt}}()
+    for grouping in groupings, (i, g₁) in enumerate(grouping), g₂ in Iterators.take(grouping, i)
+        # Here, it is not guaranteed that we will see the conjugates later on.
+        push!(this_set,
+            FastKey(exponents_sum(e, g₁, monₜe)), # KillConjugates(g₁*monₜ*ḡ₂)
+            FastKey(exponents_sum(e, g₁, monₜec)), # KillConjugates(g₁*conj(monₜ)*ḡ₂)
+            FastKey(exponents_sum(e, g₂, monₜe)), # KillConjugates(g₂*monₜ*ḡ₁)
+            FastKey(exponents_sum(e, g₂, monₜec)), # KillConjugates(g₂*conj(monₜ)*ḡ₁)
+        )
+    end
+    if !isnothing(sync)
+        lock(sync)
+        try
+            union!(mons_idx_set, this_set)
+        finally
+            unlock(sync)
+        end
+    end
+    return
+end
+
+# Out of necessity, our groupings contain abstract types. Given that the number of groupings will be manageable, but the
+# groupings themselves can be quite large, it pays off to introduce a function barrier specializing on the actual type.
+function grouping_loop(::Val{Nr}, ::Val{Nc}, ::Val{constr_is_real}, mons_idx_set, e, grouping, monₜ) where {Nr,Nc,constr_is_real}
+    if iszero(Nc)
+        args = ()
+    else
+        monₜe = KillConjugates{Nr}(exponents(monₜ))
+        if constr_is_real
+            args = (monₜe,)
+        else
+            args = (monₜe, KillConjugates{Nr}(exponents(IntConjMonomial(monₜ))))
+        end
+    end
+    nthreads = Threads.nthreads()
+    if isone(nthreads) || length(grouping) < 100
+        grouping_loop(mons_idx_set, e, (grouping,), monₜ, args, nothing)
+    else
+        # The first entry in grouping has length 1, the second length 2, ...; consider this when dividing the search space.
+        # A single task will always do n items from the beginning plus n items from the end, giving a constant number of
+        # iterations.
+        iterations = div(length(grouping), 2, RoundUp)
+        iterationsperthread = div(iterations, nthreads, RoundUp)
+        startiterations = @view(grouping[1:iterations])
+        enditerations = @view(grouping[iterations+1:end])
+        startindex = 1
+        ccall(:jl_enter_threaded_region, Cvoid, ())
+        try
+            threads = Vector{Task}(undef, nthreads)
+            sync = Threads.SpinLock()
+            @inbounds for tid in 1:nthreads
+                @views thisiter = (startiterations[startindex:min(startindex+iterationsperthread-1, iterations-1)],
+                                   enditerations[startindex:min(startindex+iterationsperthread-1, length(enditerations))])
+                startindex += iterationsperthread
+                threads[tid] = Threads.@spawn grouping_loop($mons_idx_set, $e, $thisiter, $monₜ, $args, $sync)
+            end
+            for thread in threads
+                wait(thread)
+            end
+        finally
+            ccall(:jl_exit_threaded_region, Cvoid, ())
+        end
+    end
+    return
+end
+
 # We only accept ExponentsAll; else, we cannot guarantee that all the resulting exponents will in fact be possible. In this
 # way, we don't have to check for equality of the exponents sets (or form the largest cover), which is also beneficial.
 # And converting polynomials from the MP interface will yield ExponentsAll anyway.
@@ -66,44 +174,6 @@ function merge_constraints(objective::IntPolynomial{<:Any,Nr,Nc}, zero::Abstract
     # This can quickly become disastrous if `degree` is high but the degree of the constraints is low (as then, the
     # prefactors have lots of entries), but it is not so harmful in the other regime.
 
-    # Out of necessity, our groupings contain abstract types. Given that the number of groupings will be manageable, but the
-    # groupings themselves can be quite large, it pays off to introduce a function barrier specializing on the actual type.
-    grouping_loop = @capture(((grouping, monₜ, ::Val{constr_is_real}) where {constr_is_real}) -> begin
-        if !iszero(Nc)
-            monₜe = KillConjugates{Nr}(exponents(monₜ))
-            if !constr_is_real
-                monₜec = KillConjugates{Nr}(exponents(IntConjMonomial(monₜ)))
-            end
-        end
-        for (i, g₁) in enumerate(grouping)
-            for g₂ in Iterators.take(grouping, i)
-                if iszero(Nc)
-                    push!($mons_idx_set,
-                        FastKey(monomial_index($e, g₁, monₜ, g₂)),
-                        FastKey(monomial_index(e, g₂, monₜ, g₁))
-                    )
-                elseif constr_is_real
-                    # While the prefactor is g₁*conj(g₂), we drop the conjugates anyway.
-                    # Note that no prefactor has a single conjugate component; and while the monomials might have them, the
-                    # conjugates will come up later anyway!
-                    push!(mons_idx_set,
-                        FastKey(exponents_sum(e, g₁, monₜe)), # KillConjugates(g₁*monₜ*ḡ₂)
-                        FastKey(exponents_sum(e, g₂, monₜe)), # KillConjugates(g₂*monₜ*ḡ₁)
-                    )
-                else
-                    # Here, it is not guaranteed that we will see the conjugates later on.
-                    push!(mons_idx_set,
-                        FastKey(exponents_sum(e, g₁, monₜe)), # KillConjugates(g₁*monₜ*ḡ₂)
-                        FastKey(exponents_sum(e, g₁, monₜec)), # KillConjugates(g₁*conj(monₜ)*ḡ₂)
-                        FastKey(exponents_sum(e, g₂, monₜe)), # KillConjugates(g₂*monₜ*ḡ₁)
-                        FastKey(exponents_sum(e, g₂, monₜec)), # KillConjugates(g₂*conj(monₜ)*ḡ₁)
-                    )
-                end
-            end
-        end
-        nothing
-    end)
-
     # 1./2. zero/nonneg constraints
     # Note that we can still exploit that the σⱼ must be made up of products of elements in the grouping: despite being
     # polynomials of degree deg(σⱼ) with unknown coefficients, _some_ of these must for sure be zero, namely those that cannot
@@ -117,7 +187,7 @@ function merge_constraints(objective::IntPolynomial{<:Any,Nr,Nc}, zero::Abstract
             for t in constrᵢ
                 monₜ = monomial(t)
                 for grouping in groupings
-                    grouping_loop(grouping, monₜ, Val(!isz))
+                    grouping_loop(Val(Nr), Val(Nc), Val(!isz), mons_idx_set, e, grouping, monₜ)
                 end
             end
         end
@@ -138,13 +208,13 @@ function merge_constraints(objective::IntPolynomial{<:Any,Nr,Nc}, zero::Abstract
             for t in psdᵢ[i, j]
                 monₜ = monomial(t)
                 for grouping in groupings
-                    grouping_loop(grouping, monₜ, Val(i == j))
+                    grouping_loop(Val(Nr), Val(Nc), Val(i == j), mons_idx_set, e, grouping, monₜ)
                 end
             end
         end
     end
 
     # now we need to re-cast the indices into the exponent-representations
-    @verbose_info("├ Total number of coefficients: ", length(mons_idx_set))
+    @verbose_info("└ Total number of coefficients: ", length(mons_idx_set))
     return IntMonomialVector{Nr,Nc}(e, sort!(convert.(UInt, mons_idx_set)))
 end
